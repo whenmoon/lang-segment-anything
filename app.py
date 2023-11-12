2fe7a238c354a41a98d66f63161ccccd776f2d6a
Tom Belton <tombelton@hotmail.co.uk>
2023-11-12 20:35:04 +0100

    added image

--- app.py
+++ app.py
@@ -1,76 +1,45 @@
-import os
-import warnings
-
-import gradio as gr
-import lightning as L
 import numpy as np
-from lightning.app.components.serve import ServeGradio
-from PIL import Image
-
-from lang_sam import LangSAM
-from lang_sam import SAM_MODELS
 from lang_sam.utils import draw_image
-from lang_sam.utils import load_image
+from PIL import Image
+from lang_sam import LangSAM
+from heic2png import HEIC2PNG
+import os
 
-warnings.filterwarnings("ignore")
+if __name__ == '__main__':
+    heic_img = HEIC2PNG('./IMG_4313.heic', quality=70)  # Specify the quality of the converted image
+    heic_img.save()  # The converted image will be saved as `test.png`
 
+model = LangSAM()
+image_pil = Image.open("./IMG_4313.png").convert("RGB")
+text_prompt = "beanbag"
+masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
 
-class LitGradio(ServeGradio):
+masks.shape
 
-    inputs = [
-        gr.Dropdown(choices=list(SAM_MODELS.keys()), label="SAM model", value="vit_h"),
-        gr.Slider(0, 1, value=0.3, label="Box threshold"),
-        gr.Slider(0, 1, value=0.25, label="Text threshold"),
-        gr.Image(type="filepath", label='Image'),
-        gr.Textbox(lines=1, label="Text Prompt"),
-    ]
-    outputs = [gr.outputs.Image(type="pil", label="Output Image")]
+labels = [f"{phrase} {logit:.2f}" for phrase, logit in zip(phrases, logits)]
+image_array = np.asarray(image_pil)
+image = draw_image(image_array, masks, boxes, labels)
+chosen_mask = Image.fromarray(np.uint8(image)).convert("RGB")
+chosen_mask.show()
 
-    examples = [
-        [
-            'vit_h',
-            0.36,
-            0.25,
-            os.path.join(os.path.dirname(__file__), "assets", "fruits.jpg"),
-            "kiwi",
-        ],
-        [
-            'vit_h',
-            0.3,
-            0.25,
-            os.path.join(os.path.dirname(__file__), "assets", "car.jpeg"),
-            "wheel",
-        ],
-        [
-            'vit_h',
-            0.3,
-            0.25,
-            os.path.join(os.path.dirname(__file__), "assets", "food.jpg"),
-            "food",
-        ],
-    ]
+chosen_mask = np.array(chosen_mask).astype("uint8")
+chosen_mask[chosen_mask != 0] = 255
+chosen_mask[chosen_mask == 0] = 1
+chosen_mask[chosen_mask == 255] = 0
+chosen_mask[chosen_mask == 1] = 255
 
-    def __init__(self, sam_type="vit_h"):
-        super().__init__()
-        self.ready = False
-        self.sam_type = sam_type
+# create a base blank mask
+width = 3024    
+height = 4032
+mask = Image.new("RGBA", (width, height), (0, 0, 0, 1))  # create an opaque image mask
 
-    def predict(self, sam_type, box_threshold, text_threshold, image_path, text_prompt):
-        print("Predicting... ", sam_type, box_threshold, text_threshold, image_path, text_prompt)
-        if sam_type != self.model.sam_type:
-            self.model.build_sam(sam_type)
-        image_pil = load_image(image_path)
-        masks, boxes, phrases, logits = self.model.predict(image_pil, text_prompt, box_threshold, text_threshold)
-        labels = [f"{phrase} {logit:.2f}" for phrase, logit in zip(phrases, logits)]
-        image_array = np.asarray(image_pil)
-        image = draw_image(image_array, masks, boxes, labels)
-        image = Image.fromarray(np.uint8(image)).convert("RGB")
-        return image
+# Convert mask back to pixels to add our mask replacing the third dimension
+pix = np.array(mask)
+pix[:, :, 3] = chosen_mask[..., 1]
 
-    def build_model(self, sam_type="vit_h"):
-        model = LangSAM(sam_type)
-        self.ready = True
-        return model
-
-
-app = L.LightningApp(LitGradio())
+# Convert pixels back to an RGBA image and display
+new_mask = Image.fromarray(pix, "RGBA")
+#new_mask.show()
+cwd = os.getcwd()
+#new_mask.save()
+new_mask.save(os.path.join(cwd, "final_mask.png"))
